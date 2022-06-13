dcb05d64a6fb03c85254093d137fcd408727a329
NanderSantos <nandermgsdp@gmail.com>
2022-06-13 17:28:45 -0300

    Corrected tests.

--- src/test/java/br/ufmg/watchdogs/server/test/unit/mqtt/downlink/payload/MqttDownLinkAckPayloadParserImplTest.java
+++ src/test/java/br/ufmg/watchdogs/server/test/unit/mqtt/downlink/payload/MqttDownLinkAckPayloadParserImplTest.java
@@ -15,6 +15,6 @@ public class MqttDownLinkAckPayloadParserImplTest {
 
         Assertions.assertEquals(2, byteArray.length);
         Assertions.assertEquals((byte) 0b11100101, byteArray[0]);
-        Assertions.assertEquals((byte) 0b00000010, byteArray[1]);
+        Assertions.assertEquals((byte) 0b00100000, byteArray[1]);
     }
 }
