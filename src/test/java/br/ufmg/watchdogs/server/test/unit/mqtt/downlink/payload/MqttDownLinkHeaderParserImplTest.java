caa0109d3ea20ae2dd4d1fd6b75748bd589b98bc
NanderSantos <nandermgsdp@gmail.com>
2022-06-14 11:59:02 -0300

    Corrected downlink header parser test.

--- src/test/java/br/ufmg/watchdogs/server/test/unit/mqtt/downlink/payload/MqttDownLinkHeaderParserImplTest.java
+++ src/test/java/br/ufmg/watchdogs/server/test/unit/mqtt/downlink/payload/MqttDownLinkHeaderParserImplTest.java
@@ -10,14 +10,17 @@ public class MqttDownLinkHeaderParserImplTest {
     @Test
     void shouldParseACKFrameHeader() {
 
-        byte[] payload = new byte[0];
+        byte[] payload = {
+                (byte) 0b10101010,
+                (byte) 0b10101010
+        };
 
         MqttDownLinkHeaderParserImpl header = new MqttDownLinkHeaderParserImpl(payload.length, MqttDownLinkFrameTypeImpl.DOWNLINK_FRAME_TYPE_ACK);
         byte[] byteArray = header.toByteArray();
 
         Assertions.assertEquals(0b0, byteArray[0]);
         Assertions.assertEquals(0b0, byteArray[1]);
-        Assertions.assertEquals(0b0, byteArray[1]);
+        Assertions.assertEquals(0b00000010, byteArray[2]);
     }
 
     @Test
@@ -37,14 +40,13 @@ public class MqttDownLinkHeaderParserImplTest {
     void shouldParseAnimalSyncFrameHeader() {
 
         byte[] payload = {
-                0b00000000,
+                0b00000001,
                 0b00000001,
                 0b01111111,
                 0b00111111,
                 0b00011111,
                 0b00001111,
                 0b00000111,
-                0b00000011,
         };
 
         MqttDownLinkHeaderParserImpl header = new MqttDownLinkHeaderParserImpl(payload.length, MqttDownLinkFrameTypeImpl.DOWNLINK_FRAME_TYPE_ANIMAL_SYNC);
@@ -52,6 +54,6 @@ public class MqttDownLinkHeaderParserImplTest {
 
         Assertions.assertEquals(0b0, byteArray[0]);
         Assertions.assertEquals(0b00100000, byteArray[1]);
-        Assertions.assertEquals(0b00001000, byteArray[2]);
+        Assertions.assertEquals(0b00000111, byteArray[2]);
     }
 }
